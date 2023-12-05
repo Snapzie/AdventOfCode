6fcf956fac7feff3d9673557208411a3d7a27a5f
Casper Kian Bresdahl Ibsen <casper.kian.bresdahl.ibsen@regionh.dk>
2023-12-05 10:14:27 +0100

    Solved day 5 part 1

---
+++ Day5/Day5.py
@@ -0,0 +1,133 @@
+from collections import defaultdict
+
+keys = ['Dest','Source','Range']
+f = open('./input.txt').read().strip()
+for line in f.split('\n\n'):
+    if line.startswith('seeds:'):
+        seeds = [int(x) for x in line.split(': ')[1].split()]
+    if line.startswith('seed-to-soil'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        seed2Soil = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('soil-to-fertilizer'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        soil2fert = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('fertilizer-to-water'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        fert2water = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('water-to-light'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        water2light = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('light-to-temperature'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        light2temp = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('temperature-to-humidity'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        temp2humid = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('humidity-to-location'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        humid2dest = [{k:v for k,v in zip(keys,val)} for val in vals]
+
+dests = []
+for idx in seeds:
+    for dict in seed2Soil:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in soil2fert:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in fert2water:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in water2light:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in light2temp:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in temp2humid:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in humid2dest:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break    
+    dests.append(idx)
+print(min(dests))
+
+
+# Part 2
+seedsList = []
+keys = ['Dest','Source','Range']
+f = open('./input.txt').read().strip()
+for line in f.split('\n\n'):
+    if line.startswith('seeds:'):
+        seeds = [int(x) for x in line.split(': ')[1].split()]
+        seedpairs = zip(seeds[::2],seeds[1::2])
+        for start,end in seedpairs:
+            for i in range(start,start+end):
+                seedsList.append(i)
+        # seeds = [l for l in list(range(start,end)) for start,end in zip(seeds[::2],seeds[1::2])]
+        print(seedsList)
+    if line.startswith('seed-to-soil'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        seed2Soil = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('soil-to-fertilizer'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        soil2fert = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('fertilizer-to-water'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        fert2water = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('water-to-light'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        water2light = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('light-to-temperature'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        light2temp = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('temperature-to-humidity'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        temp2humid = [{k:v for k,v in zip(keys,val)} for val in vals]
+    if line.startswith('humidity-to-location'):
+        vals = [[int(x) for x in val] for val in [x.split() for x in line.split('\n')[1:]]]
+        humid2dest = [{k:v for k,v in zip(keys,val)} for val in vals]
+
+dests = []
+dicts = [humid2dest,temp2humid,light2temp,water2light,fert2water,soil2fert,seed2Soil]
+
+
+for idx in seedsList:
+    for dict in seed2Soil:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in soil2fert:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in fert2water:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in water2light:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in light2temp:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in temp2humid:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break
+    for dict in humid2dest:
+        if dict['Source'] <= idx <= dict['Source']+dict['Range']:
+            idx = idx + (dict['Dest'] - dict['Source'])
+            break    
+    dests.append(idx)
+print(min(dests))
